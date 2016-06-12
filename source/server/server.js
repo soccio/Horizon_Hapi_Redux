30d9b460afe899ea1a0003a44a95cf7bedfd22c9
Yannick soccio <ysoccio@gmail.com>
2016-06-12 13:00:05 +0800

    Update server.js

--- source/server/server.js
+++ source/server/server.js
@@ -1,13 +1,19 @@
 import path from 'path';
 import Hapi from 'hapi';
+import h2o2 from "h2o2";
 import Inert from 'inert';
 import horizon from '@horizon/server';
 import devProps from '../../config/webpack/devProps';
 import config from '../../config/page';
 import chalk from 'chalk';
 
+/**
+ * Start Hapi server
+ */
+
 const plugins = [
-    {register: Inert} // enables serving static files (file and directory handlers)
+    {register: Inert}, // enables serving static files (file and directory handlers)
+    {register: h2o2},
 ];
 
 
@@ -36,10 +42,37 @@ server.register(plugins, (err) => {
         }
     });
 
+    /**
+     * Endpoint that proxies all GitHub API requests to https://api.github.com.
+     * This fetches the api information and can then be retrieve with
+     * fetch_isomorphic in the action creator.
 
-    const host = process.env.NODE_ENV === 'production' ? '' : `http://127.0.0.1:${devProps.webpackPort}`;
-    const bundle = `${host}/static/client.bundle.js`;
-    const styles = `${host}/static/styles.css`;
+    server.route({
+    	method: "GET",
+    	path: "/api/github/{path*}",
+    	handler: {
+    		proxy: {
+    			passThrough: true,
+    			mapUri (request, callback) {
+    				callback(null, url.format({
+    					protocol: "https",
+    					host:     "api.github.com",
+    					pathname: request.params.path,
+    					query:    request.query
+    				}));
+    			},
+    			onResponse (err, res, request, reply, settings, ttl) {
+    				reply(res);
+    			}
+    		}
+    	}
+    }); */
+
+
+
+const host = process.env.NODE_ENV === 'production' ? '' : `http://127.0.0.1:${devProps.webpackPort}`;
+const bundle = `${host}/static/client.bundle.js`;
+const styles = process.env.NODE_ENV === 'production' ? '' : `<link rel="stylesheet" type="text/css" href="${host}/static/styles.css" />`;
 
     server.route({
         method: 'GET',
@@ -48,7 +81,7 @@ server.register(plugins, (err) => {
               reply(`  <html>
                   <head>
                     <title>${config.title}</title>
-                  <!--  <link rel="stylesheet" type="text/css" href="${styles}" /> -->
+                  ${styles}
                   </head>
                   <body>
                     <div class="container"></div>
@@ -58,9 +91,8 @@ server.register(plugins, (err) => {
         }
     });
 
+
     const httpServer = server.connections.map((c) => c.listener);
-
-
     const horizonServer = horizon(httpServer, {
       auto_create_collection: true,
       auto_create_index: true,
@@ -76,10 +108,8 @@ server.register(plugins, (err) => {
     });
 
 
-
     server.start(() => {
-      console.log('Server running at:', server.info.uri);
-
+      console.info("==> ✅  Server is listening");
+      console.info("==> 🌎  Go to " + server.info.uri.toLowerCase());
     });
-
 });
