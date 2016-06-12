3daffc1a96a2fc13d8f4432ee1b84bd2c334b649
Yannick soccio <ysoccio@gmail.com>
2016-06-12 13:03:23 +0800

    Update index.js

--- source/client/index.js
+++ source/client/index.js
@@ -24,10 +24,10 @@ ReactDOM.render(
 ,reactRoot );
 
 if (module.hot) {
-        module.hot.accept('./common/components/app', () => {
+        module.hot.accept('./components/app', () => {
           // If you use Webpack 2 in ES modules mode, you can
           // use <App /> here rather than require() a <NextApp />.
-          const NextApp = require('./common/components/app').default;
+          const NextApp = require('./components/app').default;
           ReactDOM.render(
             <AppContainer>
               <Provider store={store}>
