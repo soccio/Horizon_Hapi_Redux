2d23b56775930c77c69f1f85e2272b82b8aad7eb
Yannick soccio <ysoccio@gmail.com>
2016-06-12 13:02:42 +0800

    Update routes.js

--- source/client/routes.js
+++ source/client/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import App from './common/components/app';
-import MyTodos from './common/components/mytodos';
+import App from './components/app';
+import MyTodos from './components/mytodos';
 
 /**
  * The React Routes for both the server and the client.
